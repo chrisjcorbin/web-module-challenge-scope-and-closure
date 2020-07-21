59b761a2c9a766e0f09e39dad0db6344064299d9
Chris Corbin <cjcorbin@gmail.com>
2020-07-21 08:42:58 -0700

    Code Cleanup

--- stretch.js
+++ stretch.js
@@ -7,5 +7,5 @@ function createBase(baseNum){
 }
 
 var addSix = createBase(6);
-console.log (addSix(10)); // returns 16
+console.log(addSix(10)); // returns 16
 console.log(addSix(21)); // returns 27
